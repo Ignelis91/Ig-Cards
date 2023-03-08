874d6908999af9e9ab5c3d1407af02d7f40116e3
Ignelis91 <110096365+Ignelis91@users.noreply.github.com>
2023-03-08 06:43:20 +0200

    Update server.lua

--- ig-cards/server.lua
+++ ig-cards/server.lua
@@ -1,18 +1,15 @@
-local LytisTurbut = nil
+local Lytis = nil
 
 ESX.RegisterServerCallback('ig-cards:buy', function(source, cb, item)
     local xPlayer = ESX.GetPlayerFromId(source)
     local money = xPlayer.getMoney()
     if money >= 2000 then
-        local result = MySQL.Sync.fetchAll('SELECT firstname, lastname, sex, dateofbirth FROM users WHERE identifier = @identifier', {
-            ['@identifier'] = xPlayer.identifier
-        })
-        if result[1].sex == 'm' then LytisTurbut = 'Vyras' elseif result[1].sex == 'f' then LytisTurbut = 'Moteris' end
-            local desc = "  \nVardas: " .. result[1].firstname .. " " .. result[1].lastname .. "  \nLytis: " .. LytisTurbut  .. "   \nGimimo Metai: " .. result[1].dateofbirth
+            if xPlayer.get("sex") == 'm' then Lytis = 'Vyras' elseif xPlayer.get("sex") == 'f' then Lytis = 'Moteris' end
+            local desc = "  \nVardas: " .. xPlayer.getName() .. "  \nLytis: " .. Lytis  .. "   \nGimimo Metai: " .. xPlayer.get("dateofbirth")
             xPlayer.removeMoney(2000)
             exports.ox_inventory:AddItem(source, item, 1, {description = desc})
             TriggerClientEvent('esx:showNotification', source, "Sekmingai Nusipirkote")
-            cb(playAnim)
+            cb(true)
     else 
         TriggerClientEvent('esx:showNotification', source, "Jums Nepakanka Pinigu")
     end
